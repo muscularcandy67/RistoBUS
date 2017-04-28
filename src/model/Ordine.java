842472e367f39f9640137721156346c36b90ca52
muscularcandy67 <muscularcandy67@gmail.com>
2017-04-28 09:45:30 +0200

    Modified Ordine.java

--- src/model/Ordine.java
+++ src/model/Ordine.java
@@ -10,98 +10,51 @@ import java.util.*;
  */
 public class Ordine {
 
-	/**
-	 * Default constructor
-	 */
-	public Ordine() {
-	}
-
-	/**
-	 * 
-	 */
 	private String nome_Cliente;
-
-	/**
-	 * 
-	 */
-	private Map portate;
-
-	/**
-	 * 
-	 */
+	private Map portate
 	private Menu menu;
 
-	/**
-	 * @param nome_Cliente 
-	 * @param menu
-	 */
-	public void Ordine(String nome_Cliente, Menu menu) {
+	public Ordine(String nome_Cliente, Menu menu) {
 		// TODO implement here
 	}
 
-	/**
-	 * @return
-	 */
 	public Map getElencoPortate() {
 		// TODO implement here
 		return null;
 	}
 
-	/**
-	 * @param p 
-	 * @return
-	 */
 	public boolean aggiungiPortata(Portata p) {
 		// TODO implement here
 		return false;
 	}
 
-	/**
-	 * @return
-	 */
 	public String getNome_Cliente() {
 		// TODO implement here
 		return "";
 	}
 
-	/**
-	 * @param nome_Cliente 
-	 * @return
-	 */
 	public void setNome_Cliente(String nome_Cliente) {
 		// TODO implement here
 	}
 
-	/**
-	 * @return
-	 */
 	public Menu getMenu() {
 		// TODO implement here
 		return null;
 	}
 
-	/**
-	 * @param menu 
-	 * @return
-	 */
+
 	public void setMenu(Menu menu) {
 		// TODO implement here
 	}
 
-	/**
-	 * @return
-	 */
+
 	public Map getPortate() {
 		// TODO implement here
 		return null;
 	}
 
-	/**
-	 * @param portate 
-	 * @return
-	 */
 	public void setPortate(Map portate) {
-		// TODO implement here
+			// TODO implement here
 	}
 
 }
