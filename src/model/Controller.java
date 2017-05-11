6384ec788cb25e0e4f233dfe5ef3cdbd3288298f
muscularcandy67 <muscularcandy67@gmail.com>
2017-05-11 09:46:39 +0200

    MOdified controller.java

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -2,36 +2,18 @@ package model;
 
 import java.util.*;
 
-/**
- * 
- */
+
 public interface Controller {
 
-	/**
-	 * @param ordine 
-	 * @param daMettere
-	 */
+
 	public String sostituisciPortata(Ordine ordine, Portata daMettere);
 
-	/**
-	 * 
-	 */
 	public ArrayList<Menu> getMenus();
 
-	/**
-	 * @param m 
-	 * @param nomeCliente
-	 */
 	public Ordine creaOrdine(Menu m, String nomeCliente);
 
-	/**
-	 * @param p
-	 */
 	public boolean aggiungiPortata(Portata p);
 
-	/**
-	 * @param m
-	 */
 	public boolean aggiungiMenu(Menu m);
 
 }
